1d020fc2053aefbd860014a2b28ecee90392ecca
Luiz Fernanado <75041533+luizpibo@users.noreply.github.com>
2022-07-25 20:16:37 -0300

    Update querys.ts

--- src/API/querys.ts
+++ src/API/querys.ts
@@ -1,6 +1,35 @@
 import { request } from "./datocms";
 import { gql } from "graphql-request";
-
+const getAllPages = gql`
+  {
+    allPages {
+      title
+      slug
+      content {
+        ... on SectionWithTextAndImageRecord {
+          text
+          textTitle
+          image {
+            url
+          }
+          bgImage
+          direction
+        }
+        ... on SlideRecord {
+          slides {
+            bgImage
+            direction
+            text
+            textTitle
+            image {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 const getAllSlugPages = async () => {
   const query = gql`
     {
@@ -9,13 +38,15 @@ const getAllSlugPages = async () => {
       }
     }
   `;
-  const { allPages } = await request({
-    query: query,
+  const data = await request({
+    query: getAllPages,
     variables: { limit: 10 },
   });
-  const slugs = allPages.map((line) => {
-    return line.slug;
-  });
-  return slugs;
+  console.log("todas as paginas", data);
+  console.log("Conteudo da pagina 0", data.allPages[0].content)
+  // const slugs = allPages.map((line) => {
+  //   return line.slug;
+  // });
+  return data;
 };
 export { getAllSlugPages };
