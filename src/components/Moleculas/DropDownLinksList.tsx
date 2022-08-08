1666032864374b995d4ab1cec00e88a655fca685
Luiz Fernanado <75041533+luizpibo@users.noreply.github.com>
2022-08-08 18:41:05 -0300

    bug

--- src/components/Moleculas/DropDownLinksList.tsx
+++ src/components/Moleculas/DropDownLinksList.tsx
@@ -4,14 +4,14 @@ interface IDropDownLinksList {
   linksList: [{ description: string; link: string }];
 }
 
-const DropDownLinksList:React.FC<IDropDownLinksList> = ({linksList}) => {
+const DropDownLinksList: React.FC<IDropDownLinksList> = ({ linksList }) => {
   return (
     <div className="">
       <button className="px-4 py-2">Categorias de produtos</button>
       <ul>
         {linksList.map((link) => {
           return (
-            <li>
+            <li key={`Link ${link}`}>
               <Link href={link.link}>
                 <a>{link.description}</a>
               </Link>
